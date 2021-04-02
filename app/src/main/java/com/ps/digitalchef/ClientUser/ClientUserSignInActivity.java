a88055b601a37d78e91f7924076da1cb81227819
Your Name <you@example.com>
2021-04-02 10:58:13 +0530

    first commit

---
+++ app/src/main/java/com/ps/digitalchef/ClientUser/ClientUserSignInActivity.java
@@ -0,0 +1,16 @@
+package com.ps.digitalchef.ClientUser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ps.digitalchef.R;
+
+public class ClientUserSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_user_sign_in);
+    }
+}
